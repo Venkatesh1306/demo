be036994f556361312e16250d947eec00c633c63
Venkatesh1306 <venkatesh.v5378@gmail.com>
2024-04-03 14:57:25 +0530

    v0.01

--- modbus.c
+++ modbus.c
@@ -8,7 +8,8 @@ typedef struct {
     unsigned short length;
     unsigned char unit_identifier;
     unsigned char function_code;
-    unsigned char data_payload[256]; // Adjust the size as needed
+    unsigned short start_address; 
+    unsigned short address_length;// Adjust the size as needed
 } ModbusTCP_Packet;
 
 // Function to parse Modbus TCP packet from input string
@@ -28,13 +29,18 @@ void parse_Modbus_TCP_Packet(const char* input, ModbusTCP_Packet* packet) {
     // Parse function code (byte 7)
     sscanf(input + 14, "%02hhx", &packet->function_code);
 
-    // Parse data payload (bytes 8 onwards)
-    sscanf(input + 16, "%s", packet->data_payload);
+    // Parse start_address(bytes 8 onwards)
+    sscanf(input + 16, "%04hx", &packet->start_address);
+
+    // Parse length_addr(bytes 8 onwards)
+    sscanf(input + 20, "%04hx", &packet->address_length);
 }
 
 int main() {
     char input[100]; // Assuming a maximum input length of 100 characters
     ModbusTCP_Packet packet;
+    char output[256]= {3,4,5,65,77,66,33,55,77,99,54,88,88,99,99,76};
+    char out[256];
 
     // Prompt the user for input
     printf("Enter Modbus TCP packet (in hexadecimal format, without spaces):\n");
@@ -43,14 +49,27 @@ int main() {
     // Parse the Modbus TCP packet
 
     parse_Modbus_TCP_Packet(input, &packet);
-if (strlen(input) > 12){
+if (strlen(input) >= 24){
     // Display the parsed components
     printf("Transaction Identifier: %04X\n", packet.transaction_identifier);
     printf("Protocol Identifier: %04X\n", packet.protocol_identifier);
     printf("Length: %04X\n", packet.length);
     printf("Unit Identifier: %02X\n", packet.unit_identifier);
     printf("Function Code: %02X\n", packet.function_code);
-    printf("Data Payload: %s\n", packet.data_payload);
+    printf("start address: %04X\n", packet.start_address);
+    printf("length_addr: %04X\n", packet.address_length);
+
+   unsigned short a = packet.start_address;
+    int b = packet.address_length;
+    int i;
+    char c;
+    for(i=0;i<b;i++){
+    printf("%d\n",output[a+i]);
+    }
+
+    /*if( packet.start_address == 0002){
+        printf("%d",output[2]);
+    }*/
 }
 else {
         printf("Failed to parse Modbus TCP packet. Input string is too short.\n");
