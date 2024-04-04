c683136749085a1cbb6e550aa5e60f4ab29c17a0
Venkatesh1306 <venkatesh.v5378@gmail.com>
2024-04-04 15:45:03 +0530

    v0.05

--- modbus1.c
+++ modbus1.c
@@ -6,23 +6,33 @@
 #define INPUT_SIZE 12
 
 
+#define testing
 
 #define BYTE unsigned char
 #define WORD unsigned short
 
-void test(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-}
+void in_func(BYTE input[]); 
+void res_func(WORD output[],WORD Reg[]);
 
 typedef union {
     WORD Val;
     BYTE v[2];
 } WORD_VAL;
 
-void in_func(BYTE input[]); // Prototype with input[] as an argument
+
+
+#ifdef testing
+void test(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+}
+#endif
+
+
+
+void in_func(BYTE input[]);
 void test(const char *format, ...);
 
 BYTE Reg[100];
@@ -39,7 +49,7 @@ typedef struct {
 
 Modbus parse;
 
-void in_func(BYTE input[]) { // Accept input[] as an argument
+void in_func(BYTE input[]) {
     parse.transaction_identifier.v[1]   = input[0];
     parse.transaction_identifier.v[0]   = input[1];
     parse.protocol_identifier.v[1]      = input[2];
@@ -53,6 +63,7 @@ void in_func(BYTE input[]) { // Accept input[] as an argument
     parse.address_length.v[1]           = input[10];
     parse.address_length.v[0]           = input[11]; 
 
+#ifdef testing
     test("Transaction Identifier: %04X\n", parse.transaction_identifier.Val);
     test("Protocol Identifier: %04X\n", parse.protocol_identifier.Val);
     test("Length: %04X\n", parse.length.Val);
@@ -60,11 +71,13 @@ void in_func(BYTE input[]) { // Accept input[] as an argument
     test("Function Code: %02X\n", parse.function_code);
     test("Start Address: %04X\n", parse.start_address.Val);
     test("Address Length: %04X\n", parse.address_length.Val);
+#endif
 }
 
 int main() {
-    BYTE input[100] = {0x00, 0x01, 0x00, 0x02, 0x00, 0x06, 0x00, 0x06, 0x00, 0x02, 0x00, 0x03};
-
-    in_func(input); // Pass input[] to the function
+#ifdef testing
+    BYTE input[100] = {0x00, 0x01, 0x00, 0x02, 0x00, 0x06, 0x03, 0x06, 0x00, 0x04, 0x00, 0x03};
+#endif
+    in_func(input);
     return 0;
 }
